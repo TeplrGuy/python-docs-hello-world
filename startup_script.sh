d8d6372441b67dcda3fac77ebb8d75e5a8ca1862
Gilbert <gilbertappiah@icloud.com>
2021-05-12 16:50:38 -0400

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
-# Installing cron
-apt-get update -qq && apt-get install cron -yqq
-service cron start
-mkdir /home/BackupLogs
-(crontab -l 2>/dev/null; echo "*/5 * * * * cp /home/LogFiles/*.log /home/BackupLogs")|crontab
+
+echo 'export APP_PATH="/home/site/wwwroot"' >> ~/.bashrc
+echo 'cd $APP_PATH' >> ~/.bashrc
+
+# Enter the source directory to make sure the script runs where the user expects
+cd /home/site/wwwroot
+
+export APP_PATH="/home/site/wwwroot"
+if [ -z "$HOST" ]; then
+		export HOST=0.0.0.0
+fi
+
+export PORT=8000
+
+export PATH="/opt/python//bin:${PATH}"
+echo 'export VIRTUALENVIRONMENT_PATH="/home/site/wwwroot/antenv"' >> ~/.bashrc
+echo '. antenv/bin/activate' >> ~/.bashrc
+  echo WARNING: Could not find virtual environment directory '/home/site/wwwroot/antenv'.
+  echo WARNING: Could not find package directory '/home/site/wwwroot/__oryx_packages__'.
+GUNICORN_CMD_ARGS="--timeout 600 --access-logfile '-' --error-logfile '-' --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot" gunicorn app:app
+
+
