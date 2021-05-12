5a0099ac042b4938fb4928c584b45212779f0a4e
Gilbert <gilbertappiah@icloud.com>
2021-05-12 16:44:52 -0400

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-# testing
+# Installing cron
+apt-get update -qq && apt-get install cron -yqq
+service cron start
+mkdir /home/BackupLogs
+(crontab -l 2>/dev/null; echo "*/5 * * * * cp /home/LogFiles/*.log /home/BackupLogs")|crontab
